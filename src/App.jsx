e5d7998a6fee8cd67ee90418b3c81032f289acdb
krishna <krishnasingj137333@gmail.com>
2024-02-11 00:09:01 +0530

    added mirage server

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Vans from "./pages/Vans";
+import "./server";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <header>
+        <Link className='site-logo' to='/'>
+          #VanLife
+        </Link>
+        <nav>
+          <Link to='/about'>About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/vans' element={<Vans />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className='home-container'>
+      <h1>You got to travel plans, we got the travel vans.</h1>
+      <p>
+        add adventure to your life by joining the #VanLife movement. Rent the
+        perfect van to make your perfect road trip.
+      </p>
+      <Link to='vans'>Find your van</Link>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
