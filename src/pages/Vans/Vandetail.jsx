29fc615dae85cf8115bc4299390564f83725f622
krishna <krishnasingj137333@gmail.com>
2024-02-12 00:12:12 +0530

    added header, footer and nested layout

---
+++ src/pages/Vans/Vandetail.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+const VanDetail = () => {
+  const { id } = useParams();
+  const [van, setVans] = useState(null);
+  ///console.log(id);
+
+  useEffect(() => {
+    fetch(`/api/vans/${id}`)
+      .then((res) => res.json())
+      .then((data) => setVans(data.vans));
+  }, [id]);
+
+  return (
+    <div className='van-detail-container'>
+      {van ? (
+        <div className='van-detail'>
+          <img alt={van.name} src={van.imageUrl} />
+          <i className={`van-type ${van.type} selected`}>{van.type}</i>
+          <h2>{van.name}</h2>
+          <p className='van-price'>
+            <span>${van.price}</span>/day
+          </p>
+          <p>{van.description}</p>
+          <button className='link-button'>Rent this van</button>
+        </div>
+      ) : (
+        <h2>Loading...</h2>
+      )}
+    </div>
+  );
+};
+
+export default VanDetail;
