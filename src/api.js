e28cf8e436361d0ecb45a139cba7c56f8ccb3ecb
krishna <krishnasingj137333@gmail.com>
2024-03-08 19:59:55 +0530

    Added useLoaderData react-router-dom to remove to traditional useEffect to fetch to the from external api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,12 @@
-const getVans = async () => {
-  const response = await fetch("/api/vans");
-  const data = await response.json();
-  return data.vans;
+export const getVans = async () => {
+  const res = await fetch("/api/vans")
+  if (!res.ok) {
+      throw {
+          message: "Failed to fetch vans", 
+          statusText: res.statusText,
+          status: res.status
+      }
+  }
+  const data = await res.json()
+  return data.vans
 };
-
-export { getVans };
--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,18 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { Link, useSearchParams } from "react-router-dom";
+import { Link, useLoaderData, useSearchParams } from "react-router-dom";
 import { getVans } from "../../api";
+
+export function loader() {
+  return getVans()
+}
 
 const Vans = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-
-  const [vans, setVans] = useState([]);
-  useEffect(() => {
-    const loadVans = async () => {
-      const data = await getVans();
-      setVans(data);
-    };
-    loadVans();
-  }, []);
+  //const [error, setError] = useState(null);
+  const vans = useLoaderData();
+  //console.log(vans);
 
   const typeFilter = searchParams.get("type");
 
@@ -51,6 +49,10 @@ const Vans = () => {
       </div>
     </Link>
   ));
+
+  // if (error) {
+  //   return <h1>There was an error: {error.message}</h1>;
+  // }
 
   return (
     <div className="van-list-container">
